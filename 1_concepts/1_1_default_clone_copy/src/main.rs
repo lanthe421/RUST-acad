47419e8bfeb18aa71174283dc76f253d5d0987a8
lanthe <136088285+lanthe421@users.noreply.github.com>
2026-03-24 10:54:50 +0300

    Step 1 1 default clone copy
    * Step 0: Answer 3 questions
    
    * answer next 3 questions
    
    * Answer next 7 questions
    
    * Answer all basic questions
    
    * 1.1: Implement Default, Clone and Copy traits

--- 1_concepts/1_1_default_clone_copy/src/main.rs
+++ 1_concepts/1_1_default_clone_copy/src/main.rs
@@ -1,3 +1,28 @@
+#[derive(Copy, Clone, Debug, Default)]
+struct Point {
+    x: f64,
+    y: f64,
+}
+
+#[derive(Clone, Debug)]
+struct Polyline {
+    points: Vec<Point>,
+}
+
+impl Polyline {
+    fn new(first: Point) -> Self {
+        Polyline { points: vec![first] }
+    }
+}
+
 fn main() {
-    println!("Implement me!");
+    let point1 = Point { x: 5.0, y: 7.3 };
+    let point2 = Point { x: 3.0, y: 4.3 };
+
+    let line1 = Polyline::new(point1);
+    let mut line2 = line1.clone();
+    line2.points.push(point2);
+
+    println!("{:?}", line1);
+    println!("{:?}", line2);
 }
