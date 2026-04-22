50ffde5d47b98b8ff64b527225f888415efa6ab2
lanthe <136088285+lanthe421@users.noreply.github.com>
2026-04-22 14:35:44 +0300

    Step 1.3 rc cell (#4)
    * Step 0: Answer 3 questions
    
    * answer next 3 questions
    
    * Answer next 7 questions
    
    * Answer all basic questions
    
    * 1.1: Implement Default, Clone and Copy traits
    
    * 1.2: Implement SayHi, MutMeSomehow  and MeasurableFuture with Pin
    
    * answered the questions at the end of the README file 1.2 box pin
    
    * Implemented GlobalStack and answer questions in README below
    
    * feat(1_3_rc_cell): Update stack operations and output order
    
    - Reorder pop operations to alternate between stack and stack2
    - Change final debug output from stack to stack2
    - Demonstrate shared ownership behavior with Rc and Cell types

--- 1_concepts/1_3_rc_cell/src/main.rs
+++ 1_concepts/1_3_rc_cell/src/main.rs
@@ -1,3 +1,34 @@
+use std::sync::{Arc, Mutex};
+
+#[derive(Clone, Debug)]
+struct GlobalStack<T> {
+    data: Arc<Mutex<Vec<T>>>,
+}
+
+impl<T> GlobalStack<T> {
+    fn new() -> Self {
+        GlobalStack { data: Arc::new(Mutex::new(Vec::new())) }
+    }
+
+    fn push(&self, value: T) {
+        self.data.lock().unwrap().push(value)
+    }
+
+    fn pop(&self) -> Option<T> {
+        self.data.lock().unwrap().pop()
+    }
+}
+
 fn main() {
-    println!("Implement me!");
+    let stack = GlobalStack::new();
+    let stack2 = stack.clone(); // same data, just another pointer
+
+    stack.push(1);
+    stack2.push(2);
+
+    println!("{:?}", stack.pop());
+    println!("{:?}", stack2.pop());  
+    println!("{:?}", stack2.pop());  
+    println!("{:?}", stack.pop());  
+    println!("{:?}", stack2)
 }
