48c4c7af72a4be1f5ce35099bd42900864705be9
lanthe421 <lanthe421@gmail.com>
2026-05-09 14:40:36 +0300

    added `From` and `Into` for structs

--- 4_backend/4_1_db/src/models.rs
+++ 4_backend/4_1_db/src/models.rs
@@ -1,8 +1,9 @@
 use chrono::{DateTime, Utc};
 use serde_json::Value as JsonValue;
 use uuid::Uuid;
+use derive_more::{ From, Into};
 
-#[derive(Debug, sqlx::FromRow)]
+#[derive(Debug, sqlx::FromRow, From, Into)]
 pub struct User {
     pub id: Uuid,
     pub name: String,
@@ -10,7 +11,7 @@ pub struct User {
     pub created_at: DateTime<Utc>,
 }
 
-#[derive(Debug, sqlx::FromRow)]
+#[derive(Debug, sqlx::FromRow, From, Into)]
 pub struct Role {
     pub slug: String,
     pub name: String,
